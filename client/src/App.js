47a3a903b3ede6ea110a4afe854ebc9a18d08a47
unknown <Valarie.Regas@gmail.com>
2018-01-14 15:42:17 -0500

    Auth0 early stages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+
+// App.js
+import Auth from './Auth/Auth.js';
+const auth = new Auth();
+auth.login();
 
 const App = () =>
   <Router>
