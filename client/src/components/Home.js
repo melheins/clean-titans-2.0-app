9e6ccd360fcd39611b953cde98c82a5a9070c9c6
unknown <Valarie.Regas@gmail.com>
2018-01-15 17:06:22 -0500

    working on Auth0

---
+++ client/src/components/Home.js
@@ -0,0 +1,16 @@
+// Home.jsx
+
+var Home = React.createClass({
+    // ...
+    showLock: function() {
+        // Show the Auth0Lock widget
+        this.props.lock.show();
+    },
+
+    render: function() {
+        return (
+            <div className="login-box">
+                <a onClick={this.showLock}>Sign In</a>
+            </div>);
+    }
+});
