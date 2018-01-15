cca16345ccdd56ebd42d569bc3cc1369381ef16e
melheins <heins.mel@gmail.com>
2018-01-15 14:08:03 -0500

    merge cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 //import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-=======
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
->>>>>>> 61042f4d4debc92d51c5aadbd272455f63281a02
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
