f13d10631eb8306fa3d4e26c40f552ec7166ff19
melheins <heins.mel@gmail.com>
2018-01-15 13:39:50 -0500

    Parent nav inprogress

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+//import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import ParentNav from "./components/Nav";
 
 const App = () =>
-  <Router>
-    <div>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={Books} />
-        <Route exact path="/books" component={Books} />
-        <Route exact path="/books/:id" component={Detail} />
-        <Route component={NoMatch} />
-      </Switch>
-    </div>
-  </Router>;
+    <Router>
+        <div>
+            <ParentNav/>
+            <Switch>
+                <Route exact path="/" component={Books}/>
+                <Route exact path="/books" component={Books}/>
+                <Route exact path="/books/:id" component={Detail}/>
+                <Route component={NoMatch}/>
+            </Switch>
+        </div>
+    </Router>;
 
 export default App;
---
+++ client/src/components/Nav/ParentNav.js
@@ -0,0 +1,74 @@
+import React from "react";
+//import {AppBar, Tabs, Tab} from 'material-ui';
+//import MissionBtn from "../NavBtns";
+//import missionsImage from "../../../public/images/icons/clean01.png"
+import logo from '../../assets/logo/logo_big_wide.png'
+import teamImg from '../../assets/icons/fam01.png'
+import apprIm from '../../assets/icons/fam01.png'
+import rewardsImg from '../../assets/icons/reward01.png'
+import missionsImg from '../../assets/icons/clean01.png'
+
+
+import "./Nav.css";
+
+
+const ParentNav = () =>
+    <nav className="navbar navbar-inverse navbar-top">
+        <div className="container-fluid">
+            <div className="navbar-header">
+                <button type="button" className="collapsed navbar-toggle">
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"/> <span className="icon-bar"/>
+                    <span className="icon-bar"/>
+                </button>
+                <a href="/" className="">
+                    <img className={"header-logo"} src={logo} alt="logo"/>
+                </a>
+                <ul className={"nav navbar-nav navbar-right"}>
+                    <li className={"nav-icon"}><a href="/" className={"parent_team"}>
+                        <img alt="Team"
+                             className={"nav-icon-img"}
+                             src={teamImg}/>
+                            <div className={"nav-icon-text"}>Team</div>
+                    </a></li>
+                    <li className={"nav-icon"}><a href="/" className={"parent_rewards"}>
+                        <img
+                        alt="Rewards" className={"nav-icon-img"}
+                        src={rewardsImg}/>
+                        <div className={"nav-icon-text"}>Rewards</div>
+                    </a></li>
+                    <li className={"nav-icon"}><a href="/" className={"parent_missions"}>
+                        <img
+                        alt="Missions" className={"nav-icon-img"}
+                        src={missionsImg}/>
+                        <div className={"nav-icon-text"}>Mission</div>
+                    </a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+;
+
+
+/**
+
+ const Nav = () =>
+ <nav className="navbar navbar-inverse navbar-top">
+ <div className="container-fluid">
+ <div className="navbar-header">
+ <button type="button" className="collapsed navbar-toggle">
+ <span className="sr-only">Toggle navigation</span>
+ <span className="icon-bar" /> <span className="icon-bar" />
+ <span className="icon-bar" />
+ </button>
+ <a href="/" className="navbar-brand">
+ React Reading List
+ </a>
+ </div>
+ </div>
+ </nav>;
+
+ **/
+
+
+export default ParentNav;
