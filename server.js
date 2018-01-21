968e4ec3deb371f133e7115b5fec347a97298653
unknown <Valarie.Regas@gmail.com>
2018-01-21 12:04:29 -0500

    removing redundancies

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
