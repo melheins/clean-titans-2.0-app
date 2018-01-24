596f3b8e2d54487b48634c5cfaab6c9858fccfe7
melheins <heins.mel@gmail.com>
2018-01-23 21:49:20 -0500

    parent id work

--- client/src/sections/Parent/ParentMain.js
+++ client/src/sections/Parent/ParentMain.js
@@ -7,21 +7,37 @@ import {Rewards} from "./Rewards";
 import {ParentNav} from "../../components/Nav";
 
 
-export const ParentMain = () =>
-    <Router>
-        <div>
-            <ParentNav/>
-            <div>
-                <Switch>
-                    <Route exact path="/parent" component={Approvals}/>
-                    <Route exact path="/parent/approvals" component={Approvals}/>
-                    <Route exact path="/parent/team" component={Team}/>
-                    <Route exact path="/parent/rewards" component={Rewards}/>
-                    <Route exact path="/parent/missions" component={Missions}/>
-                </Switch>
-            </div>
-        </div>
-    </Router>;
+export class ParentMain extends React.Component {
+
+    state = {
+        pid: 0
+    };
+
+    componentDidMount() {
+
+        console.log(localStorage.getItem('id_token'));
+        //localStorage.setItem
+    }
 
 
+    render() {
+        return (
+            <Router>
+                <div>
+                    <ParentNav/>
+                    <div>
+                        <Switch>
+                            <Route exact path="/parent" component={Approvals}/>
+                            <Route exact path="/parent/approvals" component={Approvals}/>
+                            <Route exact path="/parent/team" component={Team}/>
+                            <Route exact path="/parent/rewards" component={Rewards}/>
+                            <Route exact path="/parent/missions" component={Missions}/>
+                        </Switch>
+                    </div>
+                </div>
+            </Router>
+
+        )
+    }
+}
 
