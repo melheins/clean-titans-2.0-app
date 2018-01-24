9920f43a82fb427f481c7edfb5740a40bdc66572
melheins <heins.mel@gmail.com>
2018-01-23 19:51:35 -0500

    heroku testing

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Serve up static assets
-app.use(express.static("client/build"));
+//app.use(express.static("client/build"));
 
 // Add routes, both API and view
 app.use(routes);
