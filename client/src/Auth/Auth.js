991068815850c25bad2579efed9ad57e8c7669a1
melheins <heins.mel@gmail.com>
2018-01-27 13:15:45 -0500

    rebuild for heroku

--- client/src/Auth/Auth.js
+++ client/src/Auth/Auth.js
@@ -9,7 +9,7 @@ const ACCESS_TOKEN_KEY = 'access_token';
 
 const CLIENT_ID = '3j2OVQKQy5NMMVpj5Kz1KaaOHWTNLqfC';
 const CLIENT_DOMAIN = 'regas.auth0.com';
-const REDIRECT = 'http://localhost:3000/parent';
+const REDIRECT = 'https://clean-titans-2.herokuapp.com/parent';
 const SCOPE = 'openid';
 const AUDIENCE = 'https://regas.auth0.com/userinfo';
 
