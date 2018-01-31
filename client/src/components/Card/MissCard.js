c1f3683e6b7e960101581b4a399d9c8661f10d09
unknown <Valarie.Regas@gmail.com>
2018-01-30 20:29:15 -0500

    continued styling

--- client/src/components/Card/MissCard.js
+++ client/src/components/Card/MissCard.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {Card, CardTitle, CardText} from 'material-ui/Card';
+import './TeamCard.css'
 //import Divider from 'material-ui/Divider';
 //import Subheader from 'material-ui/Subheader';
 //import Avatar from 'material-ui/Avatar';
@@ -26,11 +27,12 @@ class MissCard extends Component {
             <div className={"col-lg-4 col-md-12 mb-3"}>
                 <div>
                     <Card className={"p-2"}>
-                        <CardTitle style={{padding: "8px"}}
+                        <CardTitle style={{padding: "8px",
+                                           fontFamily: "Avengeance Mightiest"}}
                                    title={this.state.title}/>
-                        <CardText style={{padding: "8px"}}>Description: {this.state.description}</CardText>
-                        <CardText style={{padding: "8px"}}>Points: {this.state.points}</CardText>
-                        <CardText><div><button data-mission-id={this.state.missionId}>Complete</button></div></CardText>
+                        <CardText style={{padding: "8px", fontFamily: "Comic Geek"}}>Description: {this.state.description}</CardText>
+                        <CardText style={{padding: "8px", fontFamily: "Comic Geek"}}>Points: {this.state.points}</CardText>
+                        <CardText><div><button style={{fontFamily: "Avengeance Mightiest"}} data-mission-id={this.state.missionId}>Complete</button></div></CardText>
                     </Card>
                 </div>
             </div>
